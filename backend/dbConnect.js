99c40b2a23a3ed9ba4aefbd6ba680aee998bf282
yakir <danielyakir01@gmail.com>
2022-10-21 15:17:56 +0300

    upload app

---
+++ backend/dbConnect.js
@@ -0,0 +1,18 @@
+// connect to mongoDB
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const connectDB = async () => {
+    dotenv.config();
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+export default connectDB;
